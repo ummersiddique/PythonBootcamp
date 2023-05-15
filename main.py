14a698e943b3d3e8c0dcf94c2d9c16c7621ae65a
Umer Siddique <ummersiddique@gmail.com>
2023-05-15 17:29:33 +0500

    operators

--- main.py
+++ main.py
@@ -4,7 +4,16 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+
 def print_hi(name):
+    fib(30)
     # Use a breakpoint in the code line below to debug your script.
     a = 12
     dummy = "Youtube"
@@ -15,8 +24,33 @@ def print_hi(name):
     print(a)
 
 
+def number():
+    print("Getting number")
+    return 5
+
+
+def number_for_sum():
+    print("Getting number for sum")
+    return 10
+
+
+def operators():
+    print(f'Sum is {5 + 5}')
+    print(f'Difference {5 - 5}')
+    print(f'Product : {5 * 5}')
+    print(f'Quotient : {5 / 5}')
+    print(f'Integer Quotient : {5 // 5}')
+    print(f'Remainder : { 5 % 2}')
+    print(f'5 ** 5 : {5 ** 5}')
+    print(f'Order : {5 + 5 * 5}')
+    print(f'Order : {number_for_sum() + 10 - number()}')
+    print("--------------------------------------------")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    operators()
     print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
